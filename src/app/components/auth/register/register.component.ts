d4be800934e0490df7338cb6e5939f921d867db4
yassine raddaoui <yassineraddaouicr7@gmail.com>
2023-11-19 15:47:51 +0100

    roles in register

--- src/app/components/auth/register/register.component.ts
+++ src/app/components/auth/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component,OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {FormArray, FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import { RegisterRequest } from 'src/app/models/RegisterRequest';
 import { AuthService } from 'src/app/services/auth/auth.service';
 
@@ -8,24 +8,44 @@ import { AuthService } from 'src/app/services/auth/auth.service';
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit{
-  constructor(private authservice:AuthService,private formBuilder: FormBuilder){
+export class RegisterComponent implements OnInit {
+  constructor(private authservice: AuthService, private formBuilder: FormBuilder) {
   }
+
   ngOnInit(): void {
     this.registerForm = this.formBuilder.group({
       firstName: ['', Validators.required],
       lastName: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(8)]],
-      role: ['', Validators.required]
-    });  }
-    registerForm!: FormGroup;
-    onSubmit(){
-      if (this.registerForm.invalid) {
-        return;
-      }
-      let roles=[];
-      roles.push(this.registerForm.value['role']);
-    this.authservice.register(new RegisterRequest(this.registerForm.value['firstName'],this.registerForm.value['lastName'],this.registerForm.value['email'],this.registerForm.value['password'],roles));
+      roles: this.formBuilder.array([], [Validators.required]),
+    });
+  }
+
+  registerForm!: FormGroup;
+
+  onSubmit() {
+    if (this.registerForm.invalid) {
+      return;
+    }
+    let roles = [];
+    roles.push(this.registerForm.value['role']);
+    this.authservice.register(new RegisterRequest(this.registerForm.value['firstName'], this.registerForm.value['lastName'], this.registerForm.value['email'], this.registerForm.value['password'], this.registerForm.value['roles']));
+  }
+
+  onCheckboxChange(e: any) {
+    const roles: FormArray = this.registerForm.get('roles') as FormArray;
+    if (e.target.checked) {
+      roles.push(new FormControl(e.target.value));
+    } else {
+      let i: number = 0;
+      roles.controls.forEach((item: any) => {
+        if (item.value == e.target.value) {
+          roles.removeAt(i);
+          return;
+        }
+        i++;
+      });
+    }
   }
 }
