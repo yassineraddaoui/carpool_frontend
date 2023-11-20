0bd81894f467d4e24039018c3e85c0c8b50a2803
Karim Ouazzou <karimouazzou@gmail.com>
2023-11-20 20:52:59 +0100

    register design & fixing rerender of main.js

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ScriptLoaderService } from './script-loader.service';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'carpool_frontend';
+  constructor(private router: Router, private scriptLoaderService: ScriptLoaderService) {}
+
+  ngOnInit() {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.scriptLoaderService.loadScript('assets/js/main.js')
+          .then(() => {
+            console.log('main.js is reloaded');
+          })
+          .catch(error => console.error('error reloading script:', error));
+      }
+    });
+  }
 }
